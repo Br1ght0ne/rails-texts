b4ec65dd58f0566163b0304f6beee388fa440aae
Oleksii Filonenko <brightone@protonmail.com>
2019-02-12 01:24:25 +0200

    Fix production mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -96,7 +96,8 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  ActionMailer::Base.smtp_settings = {
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
     user_name: ENV['sendgrid_username'],
     password: ENV['sendgrid_password'],
     domain: ENV['sendgrid_domain'],
